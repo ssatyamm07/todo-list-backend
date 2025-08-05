cbab67cfdde3e5a212606ae50b0153ae509ff6c8
Satyam Kumar <satyamkumar@Satyams-MacBook-Air.local>
2025-08-05 11:04:11 +0530

    optimized the db connection

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ import dotenv from 'dotenv';
 import taskRoutes from './routes/task.routes.js';
 import userRoutes from './routes/user.routes.js';
 import errorHandler from './middleware/error.handler.js';
-import checkDbConnection  from './config/dbcheck.js';
-
+import db from './models/index.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT;
 
-checkDbConnection();
+
+const startServer = async () => {
+  try {
+    await db.sequelize.authenticate();  
+    console.log('✅ Database connected');
+
 app.use(errorHandler); 
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 app.use('/api/auth', userRoutes);
+
+  } catch (error) {
+    console.error('❌ Unable to connect to the database:', error);
+    process.exit(1); 
+  }
+};
+
+startServer();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- config/config.cjs
+++ config/config.cjs
@@ -1,14 +1,20 @@
 require('dotenv').config();
-
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres', // Explicitly specify the dialect
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: false, // Required for NeonDB
       },
+    },
+    logging: false, 
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000, // Time Sequelize will try to get a connection before throwing error
+      idle: 10000,    // Time a connection can be idle before being released
     },
   },
 };
