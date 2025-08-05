d7b54b24d7aa88ec593584cb4b049096e096f1fd
Satyam kumar <113766540+ssatyamm07@users.noreply.github.com>
2025-08-05 19:20:56 +0530

    Recovered db optimized recovered (#8)
    * optimized the db connection
    
    ---------
    
    Co-authored-by: Satyam Kumar <satyamkumar@Satyams-MacBook-Air.local>

--- config/config.cjs
+++ config/config.cjs
@@ -2,23 +2,19 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres', // Explicitly specify the dialect
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // Required for NeonDB
+        rejectUnauthorized: false, 
       },
     },
-
     logging: false, 
     pool: {
       max: 5,
       min: 0,
-      acquire: 30000, // Time Sequelize will try to get a connection before throwing error
-      idle: 10000,    // Time a connection can be idle before being released
+      acquire: 30000, 
+      idle: 10000,    
     },
-  },
-};
-
   },
 };
